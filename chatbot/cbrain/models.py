f8aa91d5826dcd419e00a68e14093447a4ff5423
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-03-13 09:22:00 +0530

    Added doc comments

--- chatbot/cbrain/models.py
+++ chatbot/cbrain/models.py
@@ -1,7 +1,22 @@
 from django.contrib.auth.models import User
 from django.db import models
-# from django.utils.timezone import now
 
+"""
+Models for the chatbot app (chatbot/cbrain/models.py)
+
+The Chat model represents a chat session between a user and the chatbot.
+Each chat session has a unique ID, a foreign key to the user that owns it,
+and a list of messages.
+
+The Message model represents a message sent by the user or the chatbot.
+Each message has a unique ID, a foreign key to the chat session it belongs to,
+the text of the message, and the sender (either 'user' or 'bot').
+
+The Question model represents a question that the chatbot can ask the user.
+Each question has a unique ID, the text of the question, and a list of
+possible answers. The list of answers is stored as a comma-separated list
+in the database, but is exposed as a list of strings to the rest of the app.
+"""
 
 class Chat(models.Model):
     id = models.AutoField(primary_key=True)
@@ -22,7 +37,6 @@ class Message(models.Model):
     sender = models.CharField(max_length=255, default='user')
     created_at = models.DateTimeField(auto_now_add=True)
 
-
 class Question(models.Model):
     id = models.AutoField(primary_key=True)
     text = models.TextField()
@@ -35,5 +49,6 @@ class Question(models.Model):
     def set_expected_answers_list(self, value):
         self.expected_answers = ','.join(value)
 
+    # property to access the list of answers as a list
     expected_answers_list = property(
         get_expected_answers_list, set_expected_answers_list)
