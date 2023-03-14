92ebbb970bde641d28860d2ea5defd2f9f51ecd8
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-03-14 19:22:01 +0530

    Publishing commit for pythonanywhere

--- chatbot/chatbot/settings.py
+++ chatbot/chatbot/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g5_)v8@bc_z$=lia2x-kbijyrpj43%(@yzme52l!%ottk(=(12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
