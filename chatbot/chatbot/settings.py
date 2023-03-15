8b283626215746ac89d82dd744c563d8a9c9226b
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-03-15 15:02:36 +0530

    Developmental commit: WIP

--- chatbot/chatbot/settings.py
+++ chatbot/chatbot/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g5_)v8@bc_z$=lia2x-kbijyrpj43%(@yzme52l!%ottk(=(12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,11 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
