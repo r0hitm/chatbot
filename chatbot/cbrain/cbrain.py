1e697c79a0e6912298d9a6363f223ccd37872772
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-03-15 15:01:32 +0530

    replaced chatterbot with a simple chatbot written from scratch

--- chatbot/cbrain/cbrain.py
+++ chatbot/cbrain/cbrain.py
@@ -1,16 +1,137 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+import random as r
+import cbrain.questions as q
 
-# Create a new chat bot instance
-bot = ChatBot('Chatbot')
+GREETING_TOKENS = [
+    'hello',
+    'hi',
+    'greetings',
+    'konnichiwa',
+    'bonjour',
+    'hola',
+    'sup',
+    'hey',
+    'what is your name?',
+    'what\'s your name?',
+    'are you a bot?',
+    'are you a human?',
+    'hello, how are you?',
+]
 
-# Create a new trainer for the chatbot
-trainer = ChatterBotCorpusTrainer(bot)
+#  in case we don't know what to say
+DEFULT_RESPONSES = [
+    'I don\'t understand.',
+    'I don\'t know what you mean.',
+    'Please try again.',
+    'Can you rephrase that?',
+    'I don\'t know what to say.',
+]
 
-# Train the chatbot on the English corpus
-trainer.train('chatterbot.corpus.english')
+# Game variables
+INIT = False    # Flag to check if bot has been initialized
 
-# Function to get a response from the chatbot
+
+def init():
+    '''Initializes the global variables'''
+    global madlibs_responses
+    global playing
+    global asked_question
+    global asked_question_i
+    global madlib_qi
+
+    madlibs_responses = []  # stores the user's responses to the madlibs questions
+    playing = False
+    asked_question = False
+    asked_question_i = 0
+    madlib_qi = 0  # madlibs question index
+
+
+def greet():
+    '''Greets the user'''
+    return r.choice(q.greetings)
+
+
+def ask_for_playing_madlibs():
+    '''Asks the user if they want to play madlibs'''
+    return r.choice(q.ask_for_playing_madlibs)
+
+
+def ask_madlibs_question():
+    '''Returns a list of madlibs questions'''
+    global madlib_qi
+    madlib_question = q.madlibs_questions[madlib_qi]
+    madlib_qi = madlib_qi + 1
+    return madlib_question
+
+
+def goodbye():
+    '''Says goodbye to the user'''
+    return r.choice(q.goodbye)
+
+
+def ask_question():
+    '''Asks the user a question'''
+    global asked_question
+    global asked_question_i
+
+    asked_question = True
+    asked_question_i = r.randint(0, len(q.questions) - 1)
+    return q.questions[asked_question_i]
+
+
 def get_response(user_input):
-    response = bot.get_response(user_input)
-    return str(response)
+    '''Returns a response to the user's input'''
+    global asked_question
+    global INIT
+
+    if not INIT:
+        init()
+        INIT = True
+
+    user_input = user_input.lower()
+
+    if playing:
+        return play_madlibs(user_input)
+
+    if asked_question:
+        asked_question = False
+        return q.responses[asked_question_i]
+
+    if user_input in GREETING_TOKENS:
+        return greet()
+
+    elif 'play' in user_input or 'game' in user_input or 'madlibs' in user_input or 'yes' in user_input:
+        return play_madlibs('play')
+
+    elif 'goodbye' in user_input or 'bye' in user_input or 'see you later' in user_input:
+        return goodbye()
+
+    if r.random() < 0.5:
+        return ask_question()
+    else:
+        return ask_for_playing_madlibs()
+
+
+def play_madlibs(user_input=None):
+    '''Plays madlibs with the user'''
+    global madlib_qi
+    global playing
+
+    if user_input is None:
+        return ask_for_playing_madlibs()
+
+    if user_input == 'play' and not playing:
+        playing = True
+        return 'Let\'s play madlibs!\n\n' + ask_madlibs_question()
+
+    if len(user_input.split()) != 1:
+        return 'Please enter only one word as I\'m asking for'
+
+    madlibs_responses.append(user_input)
+
+    if madlib_qi == len(q.madlibs_questions):
+        playing = False
+        res = 'Here\'s your madlibs story:    ' + \
+            q.generate_madlibs(madlibs_responses)
+        return res
+
+    return ask_madlibs_question()
---
+++ chatbot/cbrain/questions.py
@@ -0,0 +1,98 @@
+import re
+# Let's play madlibs!
+
+greetings = [
+    'Hello there! How are you today?',
+    'Hi! How are you?',
+    'Hello! How are you doing?',
+    'Hello! My name is Chatbot. What is your name?',
+    'Greetings! My name is Chatbot. What is your name?',
+    'Konnichiwa! My name is Chatbot. What is your name?',
+    'Bonjour! My name is Chatbot. What is your name?',
+    'Hola! My name is Chatbot. What is your name?',
+    'Sup? My name is Chatbot. What is your name?',
+    'Hello there! excited to meet you. What is your name?',
+]
+
+ask_for_playing_madlibs = [
+    'Would you like to play a game?',
+    'Do you want to play a simple game?',
+    'Do you know what madlibs is? Let\'s play, you\'ll like it!',
+    'I have a game for you. Would you like to play?',
+]
+
+questions = [
+    'What is your favorite color?',
+    'What is your favorite food?',
+    'What is your favorite movie?',
+    'What is your favorite book?',
+    'What is your favorite video game?',
+    'What is your favorite animal?',
+    'Do you have any pets?',
+    'What is your favorite hobby?',
+    'Do you know what anime is?',
+    'What is your favorite anime?',
+    'What is your favorite sport?',
+]
+
+goodbye = [
+    'It was nice chatting with you!',
+    'Have a great day!',
+    'See you later!',
+    'Bye!',
+    'Goodbye!',
+    'Talk to you later!',
+]
+
+responses = [
+    "Hmm, that's a tough one. I like all colors equally.",
+    "I can't eat since I'm a chatbot, but I enjoy hearing about people's favorite foods.",
+    "I'm not really into movies, but I've heard good things about a lot of them.",
+    "I don't read books, but I can recommend some if you'd like, just kidding, I'm a chatbot after all.",
+    "I see, I also enjoy playing video games, but I'm not very good at them.",
+    "I love all animals, but Guinea pigs are particularly fascinating to me.",
+    "Unfortunately, I'm not capable of having pets. But I'm happy to hear about yours!",
+    "As a chatbot, I don't have hobbies. But I'm programmed to talk with people, so I guess you could say that's my favorite thing to do.",
+    "Oh, anime is genre and I've watched a few shows, Attack on Titan is my favorite.",
+    "Attack on Titan is a great show, I love it!",
+    "As a chatbot, I don't play sports. But I've heard that many people enjoy watching and playing them.",
+]
+
+# maintain the order of the questions. Very important!
+madlibs_questions = [
+    'Okay, First give me an adjective.',                # [0]
+    'Give me a noun.',                                  # [1]
+    'Give me a name.',                                  # [2]
+    'Give me a verb.',                                  # [3]
+    'Give me another noun.',                            # [4]
+    'Give me a plural noun, now.',                      # [5]
+    'Give me a verb ending in -ing.',                   # [6]
+    'Give me a place, like a city or country.',         # [7]
+    'Give me another adjective.',                       # [8]
+    'Give me another noun, yet again.',                 # [9]
+    'Give me a verb ending in -ing.',                   # [10]
+    'On more verb ending in -ing',                      # [11]
+    'Give me another adjective, you\'re doing great!',  # [12]
+    'Give me one more noun',                            # [13]
+    'Give me a verb that ends in -ed.',                 # [14]
+    'Give me one more noun',                            # [15]
+    'Give me an adjective',                             # [16]
+    'Give me another adjective, we\'re almost done',    # [17]
+    'And finally, give me a noun.',                      # [18]
+]
+
+madlibs_template = """
+One day, a {0} {1} named {2} decided to {3} to the {4} to 
+get some {5}. While {6} in the {7}, {2} suddenly saw
+a {8} {9} {10} towards them! Without {11} twice, 
+{2} grabbed a {12} {13} and {14} it at the {15}, causing
+the {1} to {14} away. {2} let out a {16} sigh of relief and
+continued to gather the {5}, feeling like a {17} {18} hero.
+"""
+
+
+def generate_madlibs(answers):
+    '''Generate madlibs from the answers (list of reponse strings)'''
+
+    madlibs = madlibs_template.format(*answers)
+    return madlibs
